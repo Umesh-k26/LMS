57ac942415a7dd93eb79ca76b37bcc434e76e3e4
tanmaygar <72341248+tanmaygar@users.noreply.github.com>
2021-04-15 20:22:03 +0530

    Add files via upload

--- CppCLR_WinformsProjekt1/Form1.h
+++ CppCLR_WinformsProjekt1/Form1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "sdf.h"
 #include "pch.h"
 #include "Form2.h"
 namespace CppCLRWinformsProjekt {
@@ -39,8 +40,8 @@ namespace CppCLRWinformsProjekt {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::TextBox^ textBox3;
-	private: System::Windows::Forms::TextBox^ textBox4;
+
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 	protected:
@@ -61,17 +62,16 @@ namespace CppCLRWinformsProjekt {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(285, 305);
+			this->button1->Location = System::Drawing::Point(428, 469);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(112, 35);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Login";
 			this->button1->UseVisualStyleBackColor = true;
@@ -79,67 +79,53 @@ namespace CppCLRWinformsProjekt {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(360, 179);
+			this->textBox1->Location = System::Drawing::Point(540, 275);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(148, 26);
 			this->textBox1->TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(360, 229);
+			this->textBox2->Location = System::Drawing::Point(540, 352);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->PasswordChar = '*';
-			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->Size = System::Drawing::Size(148, 26);
 			this->textBox2->TabIndex = 2;
-			// 
-			// textBox3
-			// 
-			this->textBox3->Location = System::Drawing::Point(142, 23);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->ReadOnly = true;
-			this->textBox3->Size = System::Drawing::Size(66, 20);
-			this->textBox3->TabIndex = 3;
-			this->textBox3->Text = L"Username - ";
-			// 
-			// textBox4
-			// 
-			this->textBox4->Location = System::Drawing::Point(160, 282);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->ReadOnly = true;
-			this->textBox4->Size = System::Drawing::Size(66, 20);
-			this->textBox4->TabIndex = 4;
-			this->textBox4->Text = L"Password - ";
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(248, 182);
+			this->label1->Location = System::Drawing::Point(375, 275);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(55, 13);
+			this->label1->Size = System::Drawing::Size(83, 20);
 			this->label1->TabIndex = 5;
 			this->label1->Text = L"Username";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(250, 232);
+			this->label2->Location = System::Drawing::Point(375, 352);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(53, 13);
+			this->label2->Size = System::Drawing::Size(78, 20);
 			this->label2->TabIndex = 6;
 			this->label2->Text = L"Password";
 			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(667, 548);
+			this->ClientSize = System::Drawing::Size(1000, 843);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
@@ -161,6 +147,8 @@ namespace CppCLRWinformsProjekt {
 			 */
 	}
 private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
--- CppCLR_WinformsProjekt1/Form2.h
+++ CppCLR_WinformsProjekt1/Form2.h
@@ -35,6 +35,13 @@ namespace CppCLR_WinformsProjekt1 {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button7;
 	protected:
 
 	private:
@@ -51,23 +58,100 @@ namespace CppCLR_WinformsProjekt1 {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(277, 148);
+			this->button1->Location = System::Drawing::Point(12, 12);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 46);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Exit";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form2::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(296, 145);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(140, 30);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"List of Books";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(296, 221);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(140, 36);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"List of Students";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(296, 294);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(179, 54);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Add Book to Library";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(296, 374);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(176, 56);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"Register a Student";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(369, 76);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(135, 43);
+			this->button6->TabIndex = 5;
+			this->button6->Text = L"Borrow History";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(110, 192);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(144, 40);
+			this->button8->TabIndex = 7;
+			this->button8->Text = L"Borrow A Book";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(86, 310);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(144, 46);
+			this->button7->TabIndex = 8;
+			this->button7->Text = L"Return A Book";
+			this->button7->UseVisualStyleBackColor = true;
 			// 
 			// Form2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(663, 528);
+			this->ClientSize = System::Drawing::Size(659, 528);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->Name = L"Form2";
