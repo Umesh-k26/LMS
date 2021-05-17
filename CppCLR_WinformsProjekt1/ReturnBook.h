a485f36f9bf150cffccabe8bc14c5ff0588b5952
Nik123-cpp <cs20btech11036@iith.ac.in>
2021-05-17 12:20:06 +0530

    Updated return Book ,By applying fine function

--- CppCLR_WinformsProjekt1/ReturnBook.h
+++ CppCLR_WinformsProjekt1/ReturnBook.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "fine_function.h"
 namespace CppCLR_WinformsProjekt1 {
 
 	using namespace System;
@@ -133,11 +134,6 @@ namespace CppCLR_WinformsProjekt1 {
 		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
 
 		MySqlCommand^ cmdDataBase1 = gcnew MySqlCommand("SELECT * FROM library_system.borrow_history WHERE order_id = '" + this->order_id_txt->Text + "';", conDataBase);
-		MySqlCommand^ cmdDataBase2 = gcnew MySqlCommand("UPDATE library_system.borrow_history set \
-        date_returned = CURDATE(),borrow_fine = 0,borrow_status = 'RETURNED'\
-		WHERE order_id = '" + this->order_id_txt->Text + "';", conDataBase);
-
-		MySqlCommand^ cmdDataBase3;
 
 		MySqlDataReader^ myReader;
 
@@ -174,10 +170,10 @@ namespace CppCLR_WinformsProjekt1 {
 
 				else if (Borrow_status = "BORROWED")
 				{
-					cmdDataBase3 = gcnew MySqlCommand("SELECT * FROM library_system.book_data WHERE book_id ='" + Book_id + "';", conDataBase);
+					MySqlCommand^ cmdDataBase2 = gcnew MySqlCommand("SELECT * FROM library_system.book_data WHERE book_id ='" + Book_id + "';", conDataBase);
 
 					int count1 = 0;
-					myReader = cmdDataBase3->ExecuteReader();
+					myReader = cmdDataBase2->ExecuteReader();
 					while (myReader->Read())
 					{
 						count1 += 1;
@@ -191,27 +187,65 @@ namespace CppCLR_WinformsProjekt1 {
 						MessageBox::Show("Book id of Borrowed book with given order_id is not found. Please resolve before updating.");
 					else if (count1 == 1)
 					{
-						MySqlCommand^ cmdDataBase4 = gcnew MySqlCommand("UPDATE library_system.book_data set copies_available = copies_available +1 WHERE \
-                      book_name = '" + Book_name + "'\
-                      AND book_author = '" + Book_author + "'\
-                      AND book_publisher = '" + Book_publisher + "'\
-                      AND book_edition_no = " + Book_edition_no + "\
-                      ; UPDATE library_system.book_data set book_borrow_status = 'AVAILABLE' WHERE book_id = " + Book_id + " ;", conDataBase);
 
-						MySqlCommand^ cmdDataBase5 = gcnew MySqlCommand("UPDATE  library_system.student_data\
-                     set student_no_book_stat = student_no_book_stat - 1 \
-                     WHERE student_id ='" + Student_id + "';", conDataBase);
+						myReader->Close();
+						MySqlCommand^ cmdDataBase3 = gcnew MySqlCommand("SELECT * FROM library_system.student_data \
+                       WHERE student_id ='" + Student_id + "';", conDataBase);
+						myReader = cmdDataBase3->ExecuteReader();
 
-						//Below Query Updates Borrow_history (Table)
-						cmdDataBase2->ExecuteNonQuery();
+						int count2 = 0;
+						String^ profession;
+						while (myReader->Read())
+						{
+							count2 += 1;
+							profession = myReader->GetString("student_profession");
+						}
+						myReader->Close();
+						if (count2 == 0)
+						{
+							MessageBox::Show("student who borrowed  book with given order_id is not found. Please resolve before updating.");
+						}
 
-						//Below Query Updates student_data(Table)
-						cmdDataBase5->ExecuteNonQuery();
+						else if(count2 == 1)
+						{
+							MySqlCommand^ cmdDataBase4 = gcnew MySqlCommand("UPDATE library_system.borrow_history set \
+                       date_returned = CURDATE(),borrow_status = 'RETURNED'\
+		               WHERE order_id = '" + this->order_id_txt->Text + "';", conDataBase);
 
-						//Below Query Updates Book_data (Table) {increases copies_available by 1 for all copies and book_borrow_Status = AVAILABLE for partciular book id}
-						cmdDataBase4->ExecuteNonQuery();
 
-						MessageBox::Show("Returned book successfully! \n book_id = " + Book_id + ",student_id = " + Student_id + ",order_id = " + this->order_id_txt->Text + " ");
+							//Below Query Updates Borrow_history (Table) except fine coloumn
+							cmdDataBase4->ExecuteNonQuery();
+
+							int order_id = Convert::ToInt32(this->order_id_txt->Text);
+							int fine = extra_func::calculate_fine(order_id, Student_id, profession);
+
+							MySqlCommand^ cmdDataBase5 = gcnew MySqlCommand("UPDATE library_system.book_data set copies_available = copies_available +1 WHERE \
+                       book_name = '" + Book_name + "'\
+                       AND book_author = '" + Book_author + "'\
+                       AND book_publisher = '" + Book_publisher + "'\
+                       AND book_edition_no = " + Book_edition_no + "\
+                       ; UPDATE library_system.book_data set book_borrow_status = 'AVAILABLE' WHERE book_id = " + Book_id + " ;", conDataBase);
+
+							MySqlCommand^ cmdDataBase6 = gcnew MySqlCommand("UPDATE  library_system.student_data\
+                       set student_no_book_stat = student_no_book_stat - 1,student_fine = student_fine + '" + fine + "' \
+                       WHERE student_id ='" + Student_id + "';", conDataBase);
+
+							MySqlCommand^ cmdDataBase7 = gcnew MySqlCommand("UPDATE library_system.borrow_history set \
+                       borrow_fine= '" + fine + "'\
+		               WHERE order_id = '" + this->order_id_txt->Text + "';", conDataBase);
+
+
+							//Below Query Updates Book_data (Table) {increases copies_available by 1 for all copies and book_borrow_Status = AVAILABLE for partciular book id}
+							cmdDataBase5->ExecuteNonQuery();
+
+							//Below Query Updates student_data(Table)
+							cmdDataBase6->ExecuteNonQuery();
+
+							//Below Query Updates fine coloumn in borrow_history by using fine function
+						    cmdDataBase7->ExecuteNonQuery();
+
+							MessageBox::Show("Returned book successfully! \n book_id = " + Book_id + ",student_id = " + Student_id + ",order_id = " + this->order_id_txt->Text + " ");
+						}
 					}
 					else if (count1 > 1)
 						MessageBox::Show("Duplicate ID's of same book_id detected. Please resolve before updating.");
